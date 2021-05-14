0a164b877a70e9b569bd716f2cdb303ccf099b62
DESKTOP-Q2CUVOB\Developer <phonethetkhin@gmail.com>
2021-05-14 21:08:52 +0630

    1.4 PTK:: Add Drawer, Modify Colors

--- app/src/main/java/com/ptkako/nv/novusvision/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/ptkako/nv/novusvision/ui/activity/HomeActivity.kt
@@ -1,19 +1,32 @@
 package com.ptkako.nv.novusvision.ui.activity
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.tabs.TabLayout
 import com.ptkako.nv.novusvision.R
 import kotlinx.android.synthetic.main.activity_home.*
 
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var tlbToolbar: Toolbar
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        tlbToolbar = findViewById(R.id.tlbToolbar)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        setSupportActionBar(tlbToolbar)
+        supportActionBar!!.title = "Novus Vision"
+        settingDrawerToggle()
         tblHomeTab.addOnTabSelectedListener(
             object : TabLayout.OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab?) {
@@ -43,4 +56,28 @@ class HomeActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val currentFragment = findNavController(R.id.nav_host_fragment).currentDestination?.id
+        if (currentFragment == R.id.homeFragment) {
+            tblHomeTab.getTabAt(0)!!.select()
+        }
+    }
+
+    private fun settingDrawerToggle() {
+        //setting the drawer toggle
+        if (supportActionBar != null) {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            toggle = ActionBarDrawerToggle(
+                this, drlHome, tlbToolbar, R.string.open, R.string.close
+            )
+            toggle.drawerArrowDrawable.color = resources.getColor(R.color.white)
+
+            drlHome.addDrawerListener(toggle)
+            toggle.syncState()
+
+        }
+    }
+
 }
